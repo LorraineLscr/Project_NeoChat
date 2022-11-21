62ab2bdd8935f388baf0f2dd998649e475423835
EpicRelax <epicrelax76@gmail.com>
2022-11-21 11:36:48 +0100

    BugFix Mentions legales

--- neochat/src/Controller/MentionsLegalesController.php
+++ neochat/src/Controller/MentionsLegalesController.php
@@ -2,19 +2,23 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ChannelRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MentionsLegalesController extends AbstractController
 {
     /**
      * @Route("/mentions/legales", name="app_mentions_legales")
      */
-    public function index(): Response
-    {
+    public function index(ChannelRepository $channelRepository): Response
+    {   
+        $channels = $channelRepository->findAll();
+        
         return $this->render('mentions_legales/index.html.twig', [
             'controller_name' => 'MentionsLegalesController',
+            'channels' => $channels ?? []
         ]);
     }
 }

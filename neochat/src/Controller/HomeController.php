b24d7d4cabef4e530151ea706dd351718706eb10
LorraineLscr <lorraiine76@hotmail.fr>
2022-11-22 13:34:25 +0100

    🎨 Vue de la page home terminée

--- neochat/src/Controller/HomeController.php
+++ neochat/src/Controller/HomeController.php
@@ -5,16 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ChannelRepository;
 
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/", name="app_home")
+     * @Route("/home", name="app_home")
      */
-    public function index(): Response
+    public function index(ChannelRepository $channelRepository): Response
     {
+        $channels = $channelRepository->findAll();
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'channels' => $channels ?? []
         ]);
     }
 }
